6ee41eefe25da8e50821b159a57c28139bdb3942
photoretouch <photoretouch@photoretouch-no-Mac-Pro.local>
2018-10-29 11:35:42 +0900

    jqueryの導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+// require_tree . //コメントアウト中
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+$(function(){ $(document).foundation(); });
